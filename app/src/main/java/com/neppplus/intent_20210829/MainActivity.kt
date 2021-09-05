b781653e99b58be1a433b8324d6aaab859912280
조경진 <cho881020@gmail.com>
2021-09-05 11:24:05 +0900

    VIEW 액션 체험   - 네이버 : 웹 링크   - 카카오톡 : 스토어 링크

--- app/src/main/java/com/neppplus/intent_20210829/MainActivity.kt
+++ app/src/main/java/com/neppplus/intent_20210829/MainActivity.kt
@@ -16,6 +16,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        kakaoStoreLinkBtn.setOnClickListener {
+            val myUri = Uri.parse("market://details?id=com.kakao.talk")
+            val myIntent = Intent( Intent.ACTION_VIEW,  myUri )
+            startActivity(myIntent)
+        }
+
+        naverLinkBtn.setOnClickListener {
+            val myUri = Uri.parse("https://naver.com")
+            val myIntent = Intent( Intent.ACTION_VIEW,  myUri )
+            startActivity(myIntent)
+        }
+
         smsBtn.setOnClickListener {
             val inputPhoneNum = phoneNumEdt.text.toString()
             val myUri = Uri.parse("smsto:${inputPhoneNum}")
